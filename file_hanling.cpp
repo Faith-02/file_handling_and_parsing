b2bfcc829d1f886ced6f3016fafd2b91945ce83a
Faith-02 <faithmichaelmbise257@gmail.com>
2025-01-01 14:49:09 +0000

    added student data structure

--- file_hanling.cpp
+++ file_hanling.cpp
@@ -6,3 +6,12 @@
 
 using namespace std;
 
+struct Student {
+    string id;
+    string name;
+    int age;
+    string grade;
+
+    string toCSV() const {
+        return id + "," + name + "," + to_string(age) + "," + grade;
+    }
